dcd113008b228c299010069e13a052c165acf30d
yuxue <yuxue1687@qq.com>
2020-05-11 09:55:10 +0800

    no commit message

--- src/main/java/com/yuxue/controller/OpencvDemo.java
+++ src/main/java/com/yuxue/controller/OpencvDemo.java
@@ -13,7 +13,7 @@ import org.opencv.core.Scalar;
  * 1、官网下载对应版本的openvp：https://opencv.org/releases/page/2/  当前使用4.0.1版本
  * 2、双击exe文件安装，将 安装目录下\build\java\x64\opencv_java401.dll 拷贝到\build\x64\vc14\bin\目录下
  * 3、eclipse添加User Libraries
- * 4、项目邮件build path，添加步骤三新增的lib
+ * 4、项目右键build path，添加步骤三新增的lib
  * 
  * 官方demo，需要本地安装opencv，除该demo之前，均不需要安装，使用maven依赖即可，
  * 二者之间具体有什么差别，暂时还没有时间去深入研究
